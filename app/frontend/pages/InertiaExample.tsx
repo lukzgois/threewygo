2698a7556dd9fc887a455df4c13d116ec797c83a
Lucas <lucaspgois@gmail.com>
2024-09-20 17:56:16 -0300

    Added docker to the project

--- app/frontend/pages/InertiaExample.tsx
+++ app/frontend/pages/InertiaExample.tsx
@@ -1,10 +1,5 @@
-import { Head } from '@inertiajs/react'
-import { useState } from 'react'
-
 export default function InertiaExample({ name }: { name: string }) {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="bg-green-100 text-4xl p-8">{name}</div>
+    <div className="bg-green-100 text-4xl p-8">Your name is: {name}</div>
   )
 }
