c4b3bef2f7021ec869c0cfd8cd01cd9a739ec034
Lucas <lucaspgois@gmail.com>
2024-09-24 04:44:33 -0300

    Showing video size on courses page

--- app/serializers/video_serializer.rb
+++ app/serializers/video_serializer.rb
@@ -1,7 +1,8 @@
 class VideoSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
+  include ActionView::Helpers::NumberHelper
 
-  attributes :id, :title, :video_url, :delete_video_url
+  attributes :id, :title, :video_url, :delete_video_url, :byte_size_formatted
 
   def video_url
     if object.video.attached?
@@ -11,5 +12,9 @@ class VideoSerializer < ActiveModel::Serializer
 
   def delete_video_url
     admin_course_video_path(object.course_id, object.id)
+  end
+
+  def byte_size_formatted
+    number_to_human_size(object.video.byte_size)
   end
 end
